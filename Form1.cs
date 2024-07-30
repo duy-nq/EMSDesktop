b1f1c38824a75441e8066ded0ca06944b588807d
Nguyen Duy <146707420+duy-nq@users.noreply.github.com>
2024-07-30 18:03:56 +0700

    feat: add function to display answer

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,10 @@ namespace EMSDesktop
 {
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
+        private RootObject RootObject;
+        private int num = 0;
+        private string[] answers = { "A", "B", "C", "D" };
+
         public Form1()
         {
             InitializeComponent();
@@ -23,12 +27,21 @@ namespace EMSDesktop
 
         private void LoadData()
         {
+            Random random = new Random();
+
+            int index = random.Next(num);
+            TxtPrediction.Text = answers[index];
+
             Thread.Sleep(3000);
         }
 
         private void simpleButton2_Click(object sender, EventArgs e)
         {
             memoEdit1.Text = OptionA.Text = OptionB.Text = OptionC.Text = OptionD.Text = "";
+            TxtAnswer.Text = "#";
+            TxtPrediction.Text = "#";
+
+            labelControl1.BackColor = Color.Transparent;
         }
 
         private void simpleButton1_Click(object sender, EventArgs e)
@@ -36,8 +49,17 @@ namespace EMSDesktop
             SplashScreenManager.ShowForm(this, typeof(WaitForm1), true, true, false);
 
             try
-            {
+            {                
                 LoadData();
+
+                if (TxtPrediction.Text == TxtAnswer.Text)
+                {
+                    labelControl1.BackColor = Color.Green;
+                }
+                else
+                {
+                    labelControl1.BackColor = Color.Red;
+                }
             }
             finally
             {
@@ -47,7 +69,31 @@ namespace EMSDesktop
 
         private void simpleButton3_Click(object sender, EventArgs e)
         {
+            TxtPrediction.Text = "#";
+            labelControl1.BackColor = Color.Transparent;
+            
+            Random random = new Random();
 
+            int index = random.Next(RootObject.Data.Count);
+
+            num = RootObject.Data[index].Choices.Length;
+
+            memoEdit1.Text = RootObject.Data[index].Question;
+
+            TxtAnswer.Text = RootObject.Data[index].Answer[0].ToString();
+
+            try
+            {
+                OptionA.Text = RootObject.Data[index].Choices[0];
+                OptionB.Text = RootObject.Data[index].Choices[1];
+                OptionC.Text = RootObject.Data[index].Choices[2];
+                OptionD.Text = RootObject.Data[index].Choices[3];
+            }
+            catch (Exception)
+            {
+                OptionD.Text = "";
+                Console.WriteLine("Error: " + RootObject.Data[index].Question);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -56,7 +102,7 @@ namespace EMSDesktop
 
             string json = File.ReadAllText(filePath);
 
-            RootObject RootObject = JsonConvert.DeserializeObject<RootObject>(json);
+            RootObject = JsonConvert.DeserializeObject<RootObject>(json);
         }
     }
 }
--- QnA.cs
+++ QnA.cs
@@ -9,19 +9,15 @@ namespace EMSDesktop
     internal class QnA
     {
         public string Question { get; set; }
-        public string OptionA { get; set; }
-        public string OptionB { get; set; }
-        public string OptionC { get; set; }
-        public string OptionD { get; set; }
+
+        public string[] Choices { get; set; }
+
         public string Answer { get; set; }
 
-        public QnA(string question, string optionA, string optionB, string optionC, string optionD, string answer)
+        public QnA(string question, string[] options, string answer)
         {
             Question = question;
-            OptionA = optionA;
-            OptionB = optionB;
-            OptionC = optionC;
-            OptionD = optionD;
+            Choices = options;
             Answer = answer;
         }
     }
